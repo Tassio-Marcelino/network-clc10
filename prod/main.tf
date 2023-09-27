d53bb9bfbe9c98ef23e2a9499c1b2232ade1275c
EC2 Default User <ec2-user@ip-10-0-1-235.ec2.internal>
2023-09-27 01:26:28 +0000

    Fix region

--- prod/main.tf
+++ prod/main.tf
@@ -2,11 +2,20 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.18.1"
+      version = "5.17.0"
     }
   }
 }
 
 provider "aws" {
+    region = "us-east-1"
+    
+    default_tags {
+        tags = {
+            Owner   = "Tasio Marcelino"
+            Env     = "Producao New Teste"
+            Class   = "CLC10"
+        }
+    }
   # Configuration options
 }
